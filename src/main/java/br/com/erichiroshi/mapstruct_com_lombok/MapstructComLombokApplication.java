dd042bcb9633622607e2d6623c78d5b2ac142cc5
erichiroshi <erichiroshi@gmail.com>
2024-06-22 09:36:06 -0400

    Projeto criado

---
+++ src/main/java/br/com/erichiroshi/mapstruct_com_lombok/MapstructComLombokApplication.java
@@ -0,0 +1,13 @@
+package br.com.erichiroshi.mapstruct_com_lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructComLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapstructComLombokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/erichiroshi/mapstruct_com_lombok/MapstructComLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erichiroshi.mapstruct_com_lombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstructComLombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
